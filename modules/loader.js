338a6a4b0b5a1e3d7ed0e644753e25a704e56454
exhaust-pipe <198379134+exhaust-pipe@users.noreply.github.com>
2026-09-15 22:19:18 +0800

    feat: load safe V5 5.2 modules in offline build

--- modules/loader.js
+++ modules/loader.js
@@ -17,6 +17,7 @@ import './mining/TunnelsMiner';
 import './foraging/AutoHarp';
 import './foraging/HideonLeafESP';
 import './foraging/HuntingHelpers';
+import './foraging/TreeESP';
 
 /* FARMING */
 import './farming/CocoaBeansMacro';
@@ -35,6 +36,7 @@ import './visuals/ProfileHider';
 import './visuals/RatESP';
 import './visuals/StructureESP';
 import './visuals/GlowingMushroomESP';
+import './visuals/RendererBenchmark';
 
 /* SKILLS */
 import './skills/AutoExperiments';
@@ -46,6 +48,9 @@ import './skills/JerryBoxMacro';
 import './skills/MinionCollector';
 import './skills/RouteWalker';
 import './skills/WynnProfessionMacro';
+
+/* BAZAAR */
+import './bazaar/BazaarNpcMacro';
 
 /* OTHER */
 import './other/AfkMacro';
