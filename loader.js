6f31b5581fc5f82c8f30718fb9c38ff25500eb50
exhaust-pipe <198379134+exhaust-pipe@users.noreply.github.com>
2026-09-15 22:30:06 +0800

    compat: initialize Render3D bridge before offline modules

--- loader.js
+++ loader.js
@@ -1,6 +1,8 @@
 Config.setAutoUpdateModules(false);
 Config.setOpenConsoleOnError(true);
 
+import './utils/RenderCompat';
+
 /* COMMANDS */
 import { registerV5Commands } from './utils/V5Commands';
 
