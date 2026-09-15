708d030443bc93487c5c3b6f4d65193c1f59c71b
exhaust-pipe <198379134+exhaust-pipe@users.noreply.github.com>
2026-09-15 22:27:00 +0800

    compat: retain legacy Executor facade

--- utils/ThreadExecutor.js
+++ utils/ThreadExecutor.js
@@ -20,6 +20,8 @@ export function executeAsync(task) {
     });
 }
 
+export const Executor = { execute: executeAsync };
+
 const shutdownExecutor = () => service.shutdownNow();
 
 register('gameUnload', shutdownExecutor);
