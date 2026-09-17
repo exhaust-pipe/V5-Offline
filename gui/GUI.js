675f0e136869836e465f596f2dabc4c516e5e3f1
exhaust-pipe <198379134+exhaust-pipe@users.noreply.github.com>
2026-09-17 22:18:14 +0800

    fix: remove stale music overlay from UI editor

--- gui/GUI.js
+++ gui/GUI.js
@@ -1,3 +1,6 @@
 import './core/GuiEvents';
 import './ProfileSettings';
 import './ThemeSettings';
+import { OverlayManager } from './OverlayUtils';
+
+OverlayManager.editorOrder = OverlayManager.editorOrder.filter((target) => target !== 'music');
